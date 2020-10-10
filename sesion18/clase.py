260de9de5fce03dae865c1911a303d7a2077469f
Kashipu <graficowm@gmail.com>
2020-10-10 00:10:16 -0500

    Fix

---
+++ sesion18/clase.py
@@ -0,0 +1,22 @@
+# Abrir un archivo de la ubicación, y lo puede leer con el argumento 'r' (read)
+
+# with open('./info.txt', 'r') as f:
+#     data = f.read()
+#     print(data)
+
+
+# escribir en un archivo de la ubicación, con el argumento 'w' (write)
+# with open('./test.txt', 'w') as f:
+#     data = 'Hola mundo \n'
+#     f.write(data)
+#     f.write(data)
+#     f.write(data)
+#     print("Ok")
+
+# with open('./test.txt', 'r') as f:
+    # print(f.read())
+    
+alejandra = "Care verga"
+
+for x in alejandra:
+    print(x)
